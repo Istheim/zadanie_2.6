662b1f0c268c26a632ca4c436b944b47648d47a2
Matvey Zaytsev(Istheim) <zaytsev_matvey98@mail.ru>
2023-08-31 02:20:05 +0700

    add all

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
-    path('blog/', include('blogs.urls'))
+    path('blog/', include('blogs.urls')),
+    path('users/', include('users.urls', namespace='users'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
